fa0ec269619cf1a84352abca2775ee19b643d858
joseglad <pjmr@live.fr>
2016-11-30 14:16:23 +0100

    Addition : show details of an event when we click on a marker

--- app/src/main/java/com/example/jihad/smartevents/MapsActivity.java
+++ app/src/main/java/com/example/jihad/smartevents/MapsActivity.java
@@ -1,25 +1,39 @@
 package com.example.jihad.smartevents;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.example.jihad.smartevents.Constantes.ConstantesActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private GoogleMap mMap;
+    private Marker myMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_maps2);
+
+        Intent intent = getIntent();
+
+        String firstName = intent.getStringExtra(ConstantesActivity.FIRST_NAME);
+        TextView firstNameText = (TextView) findViewById(R.id.userName);
+        firstNameText.setText(firstName);
+        
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -40,15 +54,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(this);
         LatLng sydney = new LatLng(-34, 151);
         LatLng event1 = new LatLng(-34.01, 151.03);
         LatLng event2 = new LatLng(-34.001, 151.025);
 
-        mMap.setMyLocationEnabled(true);
+
+        //mMap.setMyLocationEnabled(true);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12));
 
         // Add a marker in Sydney and move the camera
-        mMap.addMarker(new MarkerOptions().position(sydney)
+        myMarker = mMap.addMarker(new MarkerOptions().position(sydney)
                 .position(sydney)
                 .title("Me")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
@@ -62,10 +78,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
         mMap.addMarker(new MarkerOptions()
                 .position(event2)
-                .title("Tournoi de Badminton")
-                .snippet("Le lieu de rencontre de tous les émotions 6 places")
+                .title("Tournoi de Badminton (42 places restantes)")
+                .snippet("Le lieu de rencontre de tous les émotions")
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
 
+
+        //Toast.makeText(this, "Pfff", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        LinearLayout ll = (LinearLayout) findViewById(R.id.showEventDetails);
+
+        if(!marker.equals(this.myMarker)) {
+            TextView text = (TextView) findViewById(R.id.eventTitle);
+            ll.setVisibility(View.VISIBLE);
+
+            text.setText(marker.getTitle());
+        } else {
+            ll.setVisibility(View.GONE);
+        }
+
+        //Toast.makeText(this, marker.getTitle(), Toast.LENGTH_LONG).show();
+
+        return false;
     }
 
 }
