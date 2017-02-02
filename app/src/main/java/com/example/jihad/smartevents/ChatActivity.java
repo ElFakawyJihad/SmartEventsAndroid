35f6ee771ce25358cca119783ce284d07ca9f312
joseglad <pjmr@live.fr>
2017-02-02 15:06:27 +0100

    init chat activity

---
+++ app/src/main/java/com/example/jihad/smartevents/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.example.jihad.smartevents;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ChatActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
